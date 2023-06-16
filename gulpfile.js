294d39fbeaeb13b13a6e0f7487aa26155062344b
KScottP <kayrescott@hotmail.com>
2023-06-16 00:13:43 -0300

    styles done

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')(require('sass'))
 
-gulp.task('sass', function() {
+gulp.task('sass', gulp.series( function() {
     return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
     .pipe(sass()) //converter o Sass em CSS
     .pipe(gulp.dest('src/css')); //destino
-});
+}));
 
-gulp.task('watch', function() {
-    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], ['sass'])
-});
+gulp.task('watch', gulp.series( function() {
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.parallel( ['sass']))
+}));
 
 exports.default = gulp.series('sass', 'watch')
